1bf77e6f5a6ac9a9a4a746edc338a7f1c0332e46
oliverB_windowsHome <orribleoliver@gmail.com>
2021-11-04 09:48:06 +0000

    inital commit

---
+++ src/main/java/com/oliverB/quickTutorial/QuickTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.oliverB.quickTutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oliverB/quickTutorial/QuickTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oliverB.quickTutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
